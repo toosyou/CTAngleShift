783685ee89e7bdac4f502fef4e75f804953a17f0
toosyou <toosyou.tw@gmail.com>
2023-12-14 20:02:27 +0800

    ztilt rewritten with cuda

--- ztilt.py
+++ ztilt.py
@@ -1,89 +1,140 @@
-
+import math
 import numpy as np
-import torch
 
-from torch.nn import functional as F
+from numba import cuda
 
-class AngledFBP(torch.nn.Module):
-    ''' 
-    Filtered Backprojection
-
-    Args:
+@cuda.jit('void(float64[:, :, :], float64, float64[:], float64[:], float64[:, :, :])')
+def cuda_bp_kernel(sinogram, ztilt_theta_sin, theta_sin, theta_cos, recon):
     '''
-    def __init__(self, sinogram, circle=True, filtered=True):
-        super().__init__()
+        sinogram: (N, n_angles, M) 3d texture
+        recon: (N, M, M) 3d output
+    '''
+    z, x, y = cuda.grid(3)
+    if z >= recon.shape[0] or x >= recon.shape[1] or y >= recon.shape[2]: return
 
-        self.circle = circle
-        self.N, _, self.n_angles, self.M = sinogram.shape
+    N, n_angles, M = sinogram.shape
 
-        grid_z, grid_y, grid_x = torch.meshgrid(torch.linspace(-1, 1, self.N),
-                                                torch.linspace(-1, 1, self.M),
-                                                torch.linspace(-1, 1, self.M))
-        self.register_buffer('grid_z', torch.Tensor(grid_z).double())
-        self.register_buffer('grid_y', torch.Tensor(grid_y).double())
-        self.register_buffer('grid_x', torch.Tensor(grid_x).double())
-        self.register_buffer('sinogram', torch.Tensor(sinogram).double())
+    recon[z, x, y] = 0
+    center = M / 2
+    for i, (tsin, tcos) in enumerate(zip(theta_sin, theta_cos)):
+        sy = (x - center) * tcos - (y - center) * tsin + center
 
-        if filtered: self.apply_filter()
+        z_delta = (x - center) * tsin + (y - center) * tcos
+        sz = z + z_delta * ztilt_theta_sin
+        
+        if sy >= 0 and sy < M and sz >= 0 and sz < N:
+            # 3d interpolate
+            sy_bottom = max(0, math.floor(sy))
+            sz_bottom = max(0, math.floor(sz))
+            sy_top = min(M-1, math.ceil(sy))
+            sz_top = min(N-1, math.ceil(sz))
 
-    @staticmethod
-    def rampfilter(size):
-        n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=int),
-                            np.arange(size / 2 - 1, 0, -2, dtype=int)))
-        f = np.zeros(size)
-        f[0] = 0.25
-        f[1::2] = -1 / (np.pi * n) ** 2
-        return torch.tensor(2 * np.real(np.fft.fft(f)))
+            sy_bottom, sz_bottom = int(sy_bottom), int(sz_bottom)
+            sy_top, sz_top = int(sy_top), int(sz_top)
+            
+            c000 = sinogram[sz_bottom, i, sy_bottom]
+            c100 = sinogram[sz_top, i, sy_bottom]
+            c010 = sinogram[sz_bottom, i, sy_top]
+            c001 = sinogram[sz_bottom, i+1, sy_bottom]
+            c101 = sinogram[sz_top, i+1, sy_bottom]
+            c011 = sinogram[sz_bottom, i+1, sy_top]
+            c110 = sinogram[sz_top, i, sy_top]
+            c111 = sinogram[sz_top, i+1, sy_top]
+            
+            wy = sy - sy_bottom
+            wz = sz - sz_bottom
+            wx = i+1 - i
+            
+            c00 = c000 * (1 - wy) + c010 * wy
+            c01 = c001 * (1 - wy) + c011 * wy
+            c10 = c100 * (1 - wy) + c110 * wy
+            c11 = c101 * (1 - wy) + c111 * wy
+            
+            c0 = c00 * (1 - wz) + c01 * wz
+            c1 = c10 * (1 - wz) + c11 * wz
+            
+            c = c0 * (1 - wx) + c1 * wx
 
-    def apply_filter(self):
+            # update the reconstruction
+            recon[z, x, y] += c
+
+def rampfilter(size):
+    n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=int),
+                        np.arange(size / 2 - 1, 0, -2, dtype=int)))
+    f = np.zeros(size)
+    f[0] = 0.25
+    f[1::2] = -1 / (np.pi * n) ** 2
+    return 2 * np.real(np.fft.fft(f))
+
+def tilted_FBP(sinogram, ztilt_theta, theta, circle=True, filtered=True):
+    '''
+        sinogram: (N, n_angles, M)
+        ztilt_theta: scalar
+        theta: (n_angles,)
+    '''
+    
+    N, n_angles, M = sinogram.shape
+
+    if filtered:
         # padding values
-        final_width = max(64, int(2 ** (2 * torch.tensor(self.M)).float().log2().ceil()))
-        pad_width = (final_width - self.M)
-
-        filter = self.rampfilter(final_width)
+        final_width = max(64, int(2 ** np.ceil(np.log2(2 * M))))
+        pad_width = final_width - M
 
         # pad sinogram
-        self.sinogram = F.pad(self.sinogram, [0, pad_width], mode='constant', value=0) # (N, 1, n_angles, M + pad_width)
+        sinogram = np.pad(sinogram, [(0, 0), (0, 0), (0, pad_width)], mode='constant', constant_values=0)
 
         # apply filter
-        self.sinogram = torch.fft.fft(self.sinogram, dim=-1) * filter.double()
-        self.sinogram = torch.real(torch.fft.ifft(self.sinogram, dim=-1))[..., :self.M] # (N, 1, n_angles, M)
+        sinogram = np.fft.fft(sinogram, axis=-1) * rampfilter(final_width)
+        sinogram = np.real(np.fft.ifft(sinogram, axis=-1))[:, :, :M] 
 
-    def forward(self, tilt_theta, theta, center=None):
-        theta = torch.Tensor(theta).to(self.sinogram.device)
-        tilt_theta = torch.tensor(tilt_theta).to(self.sinogram.device)
+    # prepare theta
+    theta_sin = np.sin(theta)
+    theta_cos = np.cos(theta)
 
-        if center is not None:
-            # if center is iterable
-            if hasattr(center, '__iter__'):
-                center = torch.Tensor(center).to(self.sinogram.device)
-            else:
-                center = torch.Tensor([center] * self.N).to(self.sinogram.device)
+    # prepare ztilt_theta
+    ztilt_theta_sin = np.sin(ztilt_theta)
 
-        recon = torch.zeros(self.N, self.M, self.M, device=self.sinogram.device)
-        for i, theta_i in enumerate(theta):
-            recon_grid_x = self.grid_x * torch.cos(theta_i) - self.grid_y * torch.sin(theta_i) # (N, M, M)
-            recon_grid_x = recon_grid_x.unsqueeze(-1) # (N, M, M, 1)
+    # move to device
+    sinogram = cuda.to_device(np.ascontiguousarray(sinogram))
+    theta_sin = cuda.to_device(theta_sin)
+    theta_cos = cuda.to_device(theta_cos)
 
-            if center is not None:
-                recon_grid_x = recon_grid_x + (center.view(self.N, 1, 1, 1) - self.M / 2) / self.M * 2
+    # prepare output
+    recon = cuda.device_array((N, M, M))
 
-            z_delta = self.grid_x * torch.sin(theta_i) + self.grid_y * torch.cos(theta_i) # (N, M, M)
-            z_delta = z_delta * torch.sin(tilt_theta) * self.M / self.N
+    # prepare grid
+    threadsperblock = (1, 32, 32)
+    blockspergrid_x = math.ceil(recon.shape[0] / threadsperblock[0])
+    blockspergrid_y = math.ceil(recon.shape[1] / threadsperblock[1])
+    blockspergrid_z = math.ceil(recon.shape[2] / threadsperblock[2])
+    blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
 
-            recon_grid_z = self.grid_z + z_delta # (N, M, M)
-            recon_grid_z = recon_grid_z.unsqueeze(-1) # (N, M, M, 1)
+    # run kernel
+    cuda_bp_kernel[blockspergrid, threadsperblock](sinogram, ztilt_theta_sin, theta_sin, theta_cos, recon)
+    recon = recon.copy_to_host()
 
-            recon_grid = torch.cat((recon_grid_x, recon_grid_z), dim=-1) # (N, M, M, 2)
-            recon_grid = recon_grid.view(1, self.N * self.M, self.M, 2)
+    if circle:
+        # crop to circle for the last two dimensions
+        center = M / 2
+        x, y = np.meshgrid(np.arange(M) - center, np.arange(M) - center)
+        recon[:, ~(x ** 2 + y ** 2 <= center ** 2)] = 0
 
-            recon_i = F.grid_sample(self.sinogram[:, :, i].view(1, 1, self.N, self.M), 
-                                    recon_grid, 
-                                    mode='bilinear', 
-                                    padding_mode='zeros', 
-                                    align_corners=True) # (1, 1, N * M, M)
-            recon_i = recon_i.view(self.N, self.M, self.M) # (N, M, M)
-            recon += recon_i
+    return recon
+    
+if __name__ == '__main__':
+    import time
 
-        recon[(self.grid_x ** 2 + self.grid_y ** 2) > 0.95] = 0
-        return recon * np.pi / (2 * self.n_angles)
+    # random sinogram
+    N = 10
+    n_angles = 601
+    M = 2560
+
+    sinogram = np.random.rand(N, n_angles, M)
+    ztilt_theta = 0.5
+    theta = np.linspace(0, np.pi, n_angles)
+
+    # run cuda
+    start = time.time()
+    recon = tilted_FBP(sinogram, ztilt_theta, theta)
+    end = time.time()
+    print('cuda time:', end - start)
