a6013f44ccd47dca690be547059ca636c6d4acbf
toosyou <toosyou.tw@gmail.com>
2023-12-14 20:00:19 +0800

    code cleanup

--- ztilt.py
+++ ztilt.py
@@ -4,14 +4,6 @@ import torch
 
 from torch.nn import functional as F
 
-def rampfilter(size):
-    n = np.concatenate((np.arange(1, size / 2 + 1, 2, dtype=int),
-                        np.arange(size / 2 - 1, 0, -2, dtype=int)))
-    f = np.zeros(size)
-    f[0] = 0.25
-    f[1::2] = -1 / (np.pi * n) ** 2
-    return torch.tensor(2 * np.real(np.fft.fft(f)) )
-
 class AngledFBP(torch.nn.Module):
     ''' 
     Filtered Backprojection
