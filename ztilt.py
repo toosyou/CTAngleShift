4cc60b77166b6502ddf8d46397b25a3dde2646d5
toosyou <toosyou.tw@gmail.com>
2023-12-15 13:41:34 +0800

    refactor and move circle to cuda kernel

--- ztilt.py
+++ ztilt.py
@@ -3,8 +3,8 @@ import numpy as np
 
 from numba import cuda
 
-@cuda.jit('void(float64[:, :, :], float64, float64[:], float64[:], float64[:, :, :])')
-def cuda_bp_kernel(sinogram, ztilt_theta_sin, theta_sin, theta_cos, recon):
+@cuda.jit('void(float64[:, :, :], float64, float64[:], float64[:], float64[:, :, :], boolean)')
+def cuda_bp_kernel(sinogram, ztilt_theta_sin, theta_sin, theta_cos, recon, circle=True):
     '''
     Compute the backprojection of a tilted sinogram on GPU
 
@@ -22,44 +22,34 @@ def cuda_bp_kernel(sinogram, ztilt_theta_sin, theta_sin, theta_cos, recon):
 
     recon[z, x, y] = 0
     center = M / 2
-    for i, (tsin, tcos) in enumerate(zip(theta_sin, theta_cos)):
-        sy = (x - center) * tcos - (y - center) * tsin + center
+    x_centered = x - center
+    y_centered = y - center
+    if circle and x_centered ** 2 + y_centered ** 2 > center ** 2: return
 
-        z_delta = (x - center) * tsin + (y - center) * tcos
+    for i, (tsin, tcos) in enumerate(zip(theta_sin, theta_cos)):
+        sy = x_centered * tcos - y_centered * tsin + center
+
+        z_delta = x_centered * tsin + y_centered * tcos
         sz = z + z_delta * ztilt_theta_sin
         
         if sy >= 0 and sy < M and sz >= 0 and sz < N:
             # 3d interpolate
-            sy_bottom = max(0, math.floor(sy))
-            sz_bottom = max(0, math.floor(sz))
-            sy_top = min(M-1, math.ceil(sy))
-            sz_top = min(N-1, math.ceil(sz))
+            sy_bottom = int(max(0, math.floor(sy)))
+            sz_bottom = int(max(0, math.floor(sz)))
+            sy_top = int(min(M-1, math.ceil(sy)))
+            sz_top = int(min(N-1, math.ceil(sz)))
 
-            sy_bottom, sz_bottom = int(sy_bottom), int(sz_bottom)
-            sy_top, sz_top = int(sy_top), int(sz_top)
-            
-            c000 = sinogram[sz_bottom, i, sy_bottom]
-            c100 = sinogram[sz_top, i, sy_bottom]
-            c010 = sinogram[sz_bottom, i, sy_top]
-            c001 = sinogram[sz_bottom, i+1, sy_bottom]
-            c101 = sinogram[sz_top, i+1, sy_bottom]
-            c011 = sinogram[sz_bottom, i+1, sy_top]
-            c110 = sinogram[sz_top, i, sy_top]
-            c111 = sinogram[sz_top, i+1, sy_top]
-            
+            c0 = sinogram[sz_bottom, i, sy_bottom]
+            c1 = sinogram[sz_top, i, sy_bottom]
+            c2 = sinogram[sz_bottom, i, sy_top]
+            c3 = sinogram[sz_top, i, sy_top]
+
             wy = sy - sy_bottom
             wz = sz - sz_bottom
-            wx = i+1 - i
-            
-            c00 = c000 * (1 - wy) + c010 * wy
-            c01 = c001 * (1 - wy) + c011 * wy
-            c10 = c100 * (1 - wy) + c110 * wy
-            c11 = c101 * (1 - wy) + c111 * wy
-            
-            c0 = c00 * (1 - wz) + c01 * wz
-            c1 = c10 * (1 - wz) + c11 * wz
-            
-            c = c0 * (1 - wx) + c1 * wx
+
+            c00 = c0 * (1 - wy) + c2 * wy
+            c10 = c1 * (1 - wy) + c3 * wy
+            c = c00 * (1 - wz) + c10 * wz
 
             # update the reconstruction
             recon[z, x, y] += c
@@ -98,12 +88,13 @@ def tilted_FBP(sinogram, ztilt_theta, theta, circle=True, filtered=True):
         sinogram = np.fft.fft(sinogram, axis=-1) * rampfilter(final_width)
         sinogram = np.real(np.fft.ifft(sinogram, axis=-1))[:, :, :M] 
 
-    # move to device
-    sinogram = cuda.to_device(np.ascontiguousarray(sinogram))
-    theta_sin, theta_cos = cuda.to_device(np.sin(theta)), cuda.to_device(np.cos(theta))
+    # Apply CUDA stream
+    stream = cuda.stream()  # Create a new CUDA stream
+    sinogram = cuda.to_device(np.ascontiguousarray(sinogram), stream)
+    theta_sin, theta_cos = cuda.to_device(np.sin(theta), stream), cuda.to_device(np.cos(theta), stream)
 
     # prepare output
-    recon = cuda.device_array((N, M, M))
+    recon = cuda.device_array((N, M, M), stream=stream)
 
     # prepare grid
     threadsperblock = (1, 32, 32)
@@ -113,8 +104,15 @@ def tilted_FBP(sinogram, ztilt_theta, theta, circle=True, filtered=True):
     blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
 
     # run kernel
-    cuda_bp_kernel[blockspergrid, threadsperblock](sinogram, np.sin(ztilt_theta), theta_sin, theta_cos, recon)
-    recon = recon.copy_to_host()
+    cuda_bp_kernel[blockspergrid, threadsperblock, stream](sinogram, np.sin(ztilt_theta), theta_sin, theta_cos, recon)
+    # This will initiate the copy back to host, but will return control to CPU immediately
+    recon_host_future = recon.copy_to_host(stream=stream)
+
+    # Make sure we wait for the stream to complete before accessing on the host
+    stream.synchronize()
+
+    # Now it's safe to access
+    recon = recon_host_future
 
     if circle:
         # crop to circle for the last two dimensions
@@ -124,6 +122,70 @@ def tilted_FBP(sinogram, ztilt_theta, theta, circle=True, filtered=True):
 
     return recon
     
+class TiltedFBP:
+    def __init__(self, sinogram, circle=True, filtered=True):
+        '''
+        Compute the backprojection of a tilted sinogram
+
+        args:
+            sinogram: (N, n_angles, M) sinogram
+            circle: bool, whether to crop the reconstruction to a circle
+            filtered: bool, whether to apply ramp filter
+        '''
+        self.N, self.n_angles, self.M = sinogram.shape
+
+        self.sinogram = sinogram
+        self.circle = circle
+        self.filtered = filtered
+        self.stream = None
+        self.recon = None
+
+        self.threadsperblock = (1, 32, 32)
+        blockspergrid_x = math.ceil(self.N / self.threadsperblock[0])
+        blockspergrid_y = math.ceil(self.M / self.threadsperblock[1])
+        blockspergrid_z = math.ceil(self.M / self.threadsperblock[2])
+        self.blockspergrid = (blockspergrid_x, blockspergrid_y, blockspergrid_z)
+
+        if filtered: self.apply_filter()
+
+        self.to_device()
+
+    def apply_filter(self):
+        # padding values
+        final_width = max(64, int(2 ** np.ceil(np.log2(2 * self.M))))
+        pad_width = final_width - self.M
+
+        # pad sinogram
+        self.sinogram = np.pad(self.sinogram, [(0, 0), (0, 0), (0, pad_width)], mode='constant', constant_values=0)
+
+        # apply filter
+        self.sinogram = np.fft.fft(self.sinogram, axis=-1) * rampfilter(final_width)
+        self.sinogram = np.real(np.fft.ifft(self.sinogram, axis=-1))[:, :, :self.M]
+
+    def to_device(self):
+        self.stream = cuda.stream()  # Create a new CUDA stream
+        self.sinogram = cuda.to_device(np.ascontiguousarray(self.sinogram), self.stream)
+        self.recon = cuda.device_array((self.N, self.M, self.M), stream=self.stream)
+
+    def run(self, ztilt_theta, theta, copy_to_host=True):
+        theta_sin = cuda.to_device(np.sin(theta), self.stream)
+        theta_cos = cuda.to_device(np.cos(theta), self.stream)
+
+        # run kernel
+        cuda_bp_kernel[self.blockspergrid, self.threadsperblock, self.stream](self.sinogram, np.sin(ztilt_theta), theta_sin, theta_cos, self.recon, self.circle)
+        # This will initiate the copy back to host, but will return control to CPU immediately
+        if copy_to_host:
+            recon_host_future = self.recon.copy_to_host(stream=self.stream)
+
+        # Make sure we wait for the stream to complete before accessing on the host
+        self.stream.synchronize()
+
+        # Now it's safe to access
+        if copy_to_host:
+            self.recon = recon_host_future
+
+        return self.recon
+
 if __name__ == '__main__':
     import time
 
@@ -136,8 +198,11 @@ if __name__ == '__main__':
     ztilt_theta = 0.5
     theta = np.linspace(0, np.pi, n_angles)
 
-    # run cuda
+    # time both itialization and computation
     start = time.time()
-    recon = tilted_FBP(sinogram, ztilt_theta, theta)
+    tiltedfbp = TiltedFBP(sinogram)
+    initial_time = time.time()
+    recon = tiltedfbp.run(ztilt_theta, theta)
     end = time.time()
-    print('cuda time:', end - start)
+    print('initialization time:', initial_time - start)
+    print('computation time:', end - initial_time)
